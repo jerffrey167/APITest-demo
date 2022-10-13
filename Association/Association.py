86d515eab7048b5e03ca219006d6450014335c5b
jerffrey <jerffrey_167@163.com>
2022-10-14 00:55:27 +0800

    增加业务关联

---
+++ Association/Association.py
@@ -0,0 +1,31 @@
+import requests
+
+url = "http://120.46.215.163:8101"
+header ={"Content-Type":"application/json"}
+#########################################
+Userlogin_api ="/exam/api/sys/user/login"
+Userlogin_data = {
+				"password": "123456",
+				"username": "s2"
+				}
+login_res = requests.post(url=url+Userlogin_api, json=Userlogin_data,headers=header)
+Login_token = login_res.json()["data"]["token"]
+print("token:",Login_token)
+###############################################
+header["token"] = Login_token
+#token 赋值
+##################################################
+Exam_online_paging_api="/exam/api/exam/exam/online-paging"
+Exam_online_paging_data={
+						"current":"1",
+						 "size":"10",
+	 					"params":"",
+						 "t":"1664197427420"
+						 }
+
+Exam_online_paging_res= requests.post(url=url+Exam_online_paging_api,json=Exam_online_paging_data,headers=header)
+print(Exam_online_paging_res.text)
+
+ExamTitle = Exam_online_paging_res.json()["data"]["records"][1]["title"]
+
+print(ExamTitle)
