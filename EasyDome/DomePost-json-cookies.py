7b2486aab47ed70e408c6b4354064c36ff0dfa3d
jerffrey <jerffrey_167@163.com>
2022-10-13 17:42:56 +0800

    调整 cookies

--- EasyDome/DomePost-json-cookies.py
+++ EasyDome/DomePost-json-cookies.py
@@ -5,8 +5,9 @@ import  requests
 
 url = "http://120.46.215.163/home-favorites"
 
-headers = {"Content-Type":"application/json"}
+
 cookie= {"PHPSESSID":"lug1q02l2kak6vqjcia0oqdap8"}
 res = requests.get(url=url,cookies=cookie)
 #将字典格式的data变量转换为合法的JSON字符串传给post的data参数
 print(res.text)
+
