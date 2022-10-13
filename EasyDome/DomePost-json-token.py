c303d31cded2620039c85f5be6bdc8e770199cdc
jerffrey <jerffrey_167@163.com>
2022-10-13 16:54:38 +0800

    调整 token 接口请求

--- EasyDome/DomePost-json-token.py
+++ EasyDome/DomePost-json-token.py
@@ -3,15 +3,11 @@ import json
 
 import  requests
 
-url = "http://120.46.215.163:8101/exam/api/sys/depart/list"
-data = {
-		"deptCode": "",
-		"deptName": "",
-		"deptType": 0,
-		"id": "",
-		"parentId": "",
-		"sort": 0
-        }
+url = "http://120.46.215.163:8101/exam/api/sys/depart/paging"
+data = {"current":1,
+		"size":10,
+		"params":{"deptName":"后勤"}
+		}
 headers = {"Content-Type":"application/json",
 		   "token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjU3MzU2NjMsInVzZXJuYW1lIjoiYWRtaW4ifQ.3vBEiUq2h4cwXzwWB-PXrFXCHxru_la3dZTPAg5cOFo"}
 # 严格来说，我们需要在请求头里声明我们发送的格式
