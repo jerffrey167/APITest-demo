745039827231ef867d0eb0ea548fa48d8ea8bd9f
jerffrey <jerffrey_167@163.com>
2022-10-14 00:39:03 +0800

    添加cookies 关联

---
+++ Association/AssociationCookies.py
@@ -0,0 +1,19 @@
+import requests
+
+url = "http://120.46.215.163/"
+#########################################
+shop_res=requests.session()
+Userlogin_api ="shop-user"
+Userlogin_data = {"shopCsrf":"3004a4f1307daf2829e0f96b177655b4-522dbebcf50dbe1819946f29d6c60e07",
+				"loginAccount":"jerffrey",
+				"loginPassword":"12345678",
+				"httpReferer":""}
+login_res =shop_res.post(url=url+Userlogin_api, data=Userlogin_data)
+
+# print(login_res.text)
+# print(login_res.cookies)
+cookie = login_res.cookies
+########################
+shop_home_favorites_api="home-favorites"
+home_favorites = shop_res.get(url=url,cookies=cookie)
+print(home_favorites.text)
