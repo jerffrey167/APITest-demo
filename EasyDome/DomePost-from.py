d274aaad9fb52a0cf22bf80db040f8e5a91e0366
jerffrey <jerffrey_167@163.com>
2022-10-13 15:59:32 +0800

    debug 删除 headers 多余参数

--- EasyDome/DomePost-from.py
+++ EasyDome/DomePost-from.py
@@ -3,12 +3,12 @@ import requests
 
 
 url = "https://api.binstd.com/train/station2s"
-BodyData = {"start": "天津",
+data = {"start": "天津",
         "end": "北京",
         "ishigh":"0",
         "appkey":"b617c827636c6bb5"
         }
 # Post请求发送的数据，字典格式
-headers = {"Content-Type: multipart/form-data; boundary=--------------------------522616154642272438893363"}
-res = requests.post(url=url, data=BodyData,headers=headers) # 这里使用post方法，参数和get方法一样
+headers = {"Content-Type: multipart/form-data;"}
+res = requests.post(url=url, data=data) # 这里使用post方法，参数和get方法一样
 print(res.text)
