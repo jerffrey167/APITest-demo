9b630b020826b6f3422d3d3c0df4b066a5ebb7a5
jerffrey <jerffrey_167@163.com>
2022-10-13 23:29:22 +0800

    新增token 关联脚本2

--- Association/AssociationToken.py
+++ Association/AssociationToken.py
@@ -8,7 +8,7 @@ Userlogin_data = {
 	"password": "123456",
 	"username": "s2"
 		}
-login_res = requests.post(url=url+Userlogin_api, json=Userlogin_data)
+login_res = requests.post(url=url+Userlogin_api, json=Userlogin_data,headers=header)
 Login_token = login_res.json()["data"]["token"]
 print("token:",Login_token)
 ###############################################
@@ -18,5 +18,4 @@ header["token"] = Login_token
 Exam_Detail_api="/exam/api/exam/exam/detail"
 Exam_Detail_data={"id":"1356425140212076545"}
 Exam_Detail_res= requests.post(url=url+Exam_Detail_api,json=Exam_Detail_data,headers=header)
-
-print(Exam_Detail_res.json())
+print(Exam_Detail_res.text)
