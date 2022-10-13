86a9f270c74342a81a0bb93b954b0c8cf7f2ddd1
jerffrey <jerffrey_167@163.com>
2022-10-13 21:43:33 +0800

    增加json的传递

--- EasyDome/DomePost-json.py
+++ EasyDome/DomePost-json.py
@@ -11,6 +11,10 @@ data = {
 # 字典格式，方便添加和修改
 headers = {"Content-Type":"application/json"}
 # 严格来说，我们需要在请求头里声明我们发送的格式
-res = requests.post(url=url, data=json.dumps(data),headers=headers)
+
 #将字典格式的data变量转换为合法的JSON字符串传给post的data参数
+#res = requests.post(url=url, data=json.dumps(data),headers=headers)
+
+res = requests.post(url=url,json=data,headers=headers)
+#将字典格式的data变量直接作为JSON字符串传给post的json参数
 print(res.text)
