64fbdc3bbea21ed1f270e204248ea97817b5238b
jerffrey <jerffrey_167@163.com>
2022-10-14 10:19:54 +0800

    调整post请求的 cookies

--- EasyDome/DomePost-from.py
+++ EasyDome/DomePost-from.py
@@ -12,3 +12,16 @@ data = {"start": "天津",
 headers = {"Content-Type: multipart/form-data;"}
 res = requests.post(url=url, data=data) # 这里使用post方法，参数和get方法一样
 print(res.text)
+
+
+
+url2 = "http://120.46.215.163/shop-cart/add"
+data2 = {
+        "goodsId":"7",
+        "buyNum":"1"
+        }
+headers2 = {"Content-Type":"application/x-www-form-urlencoded;"}
+cookies =  {"PHPSESSID":"8lofdu6tj3tp0g6vc1jjf9rvmj"}
+res2 = requests.post(url=url2,data=data2,headers=headers2,cookies=cookies)
+
+print(res2.text)
