194de035d819fa48497f019126e48b8dcbb44ac5
jerffrey <jerffrey_167@163.com>
2022-10-14 14:07:13 +0800

    增加断言

---
+++ Association/AssertJson.py
@@ -0,0 +1,20 @@
+import requests
+url = "http://120.46.215.163:8101"
+header ={"Content-Type":"application/json"}
+#########################################
+Userlogin_api ="/exam/api/sys/user/login"
+Userlogin_data = {
+	"password": "123456",
+	"username": "s2"
+		}
+login_res = requests.post(url=url+Userlogin_api, json=Userlogin_data,headers=header)
+Login_token = login_res.json()["data"]["token"]
+###############################################
+header["token"] = Login_token
+#token 赋值
+##################################################
+Exam_Detail_api="/exam/api/exam/exam/detail"
+Exam_Detail_data={"id":"1356425140212076545"}
+Exam_Detail_res= requests.post(url=url+Exam_Detail_api,json=Exam_Detail_data,headers=header)
+# print(Exam_Detail_res.text)
+assert Exam_Detail_res.json()["data"]["repoList"][0]["examId"]=="1356425140212076545"
