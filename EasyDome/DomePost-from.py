4a98c596f634b901edda1ac7b807a66e42cb6f5b
jerffrey <jerffrey_167@163.com>
2022-10-14 10:51:44 +0800

    内容微调

--- EasyDome/DomePost-from.py
+++ EasyDome/DomePost-from.py
@@ -2,16 +2,16 @@ import requests
 #传统表单类POST请求（x-www-form-urlencoded）
 
 
-url = "https://api.binstd.com/train/station2s"
-data = {"start": "天津",
-        "end": "北京",
-        "ishigh":"0",
-        "appkey":"b617c827636c6bb5"
-        }
-# Post请求发送的数据，字典格式
-headers = {"Content-Type: multipart/form-data;"}
-res = requests.post(url=url, data=data) # 这里使用post方法，参数和get方法一样
-print(res.text)
+# url = "https://api.binstd.com/train/station2s"
+# data = {"start": "天津",
+#         "end": "北京",
+#         "ishigh":"0",
+#         "appkey":"b617c827636c6bb5"
+#         }
+# # Post请求发送的数据，字典格式
+# headers = {"Content-Type: multipart/form-data;"}
+# res = requests.post(url=url, data=data) # 这里使用post方法，参数和get方法一样
+# print(res.text)
 
 
 
--- EasyDome/DomePost-json-cookies.py
+++ EasyDome/DomePost-json-cookies.py
@@ -6,7 +6,7 @@ import  requests
 url = "http://120.46.215.163/home-favorites"
 
 
-cookie= {"PHPSESSID":"lug1q02l2kak6vqjcia0oqdap8"}
+cookie= {"PHPSESSID":"8r3dtv8jpisp43mp4drkqptvic"}
 res = requests.get(url=url,cookies=cookie)
 #将字典格式的data变量转换为合法的JSON字符串传给post的data参数
 print(res.text)
