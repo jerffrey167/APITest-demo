550d26f75b6ca29a18f902485958855f5a2e6a7c
jerffrey <jerffrey_167@163.com>
2022-10-14 11:53:06 +0800

    cookies 关联debug

--- Association/AssociationCookies.py
+++ Association/AssociationCookies.py
@@ -1,18 +1,21 @@
 import requests
 
-url = "http://120.46.215.163/"
+url = "http://120.46.215.163:8080"
 #########################################
-Userlogin_api ="shop-user"
-Userlogin_data = {"shopCsrf":"3004a4f1307daf2829e0f96b177655b4-522dbebcf50dbe1819946f29d6c60e07",
-				"loginAccount":"jerffrey",
-				"loginPassword":"12345678",
-				"httpReferer":""}
+Userlogin_api ="/exam/user.action"
+Userlogin_data = {
+		"username":"ww",
+		"userpwd":"12345"
+			}
 login_res =requests.post(url=url+Userlogin_api, data=Userlogin_data)
 
 # print(login_res.text)
 # print(login_res.cookies)
 cookie = login_res.cookies
+
 ########################
-shop_home_favorites_api="home-favorites"
-home_favorites = requests.get(url=url,cookies=cookie)
+
+shop_home_favorites_api="/exam/user/studentPaper/stupaper.action"
+
+home_favorites = requests.get(url=url+shop_home_favorites_api,cookies=cookie)
 print(home_favorites.text)
