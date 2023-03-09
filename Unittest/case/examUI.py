49a06d1ee88ab844e023439f05f2b851bc604f61
jerffrey <jerffrey_167@163.com>
2023-03-09 21:58:37 +0800

    no

--- Unittest/case/examUI.py
+++ Unittest/case/examUI.py
@@ -13,18 +13,18 @@ class Test_UI(unittest.TestCase):
         driver.implicitly_wait(5)
         driver.get('http://120.46.215.163:8102/#/login')
 
-    def test_001login(self):
+    def test_003login(self):
         driver.find_element(By.XPATH, '//input[@type="text"]').send_keys('admin')
         driver.find_element(By.XPATH, '//input[@type="password"]').send_keys('admin')
         driver.find_element(By.XPATH, '//button[@type="button"]').click()
 
 
-    def test_002PageOpen(self):
+    def test_004PageOpen(self):
         driver.find_element(By.XPATH, "//*[starts-with(@style,'padding-left: 20px')]/span[text()='考试管理']").click()
         sleep(1)
         driver.find_element(By.XPATH, "//*[starts-with(@style,'padding-left: 40px')]/span[text()='考试管理']").click()
 
-    def test_003Lingout(self):
+    def test_005Lingout(self):
         driver.find_element(By.XPATH, "//div[text()=' 超管A ']").click()
         sleep(1)
         driver.find_element(By.XPATH, "//span[text()='退出登录']//..").click()
