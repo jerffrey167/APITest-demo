89426d3ed0a054792488fe972fbb8b49da10c872
jerffrey <jerffrey_167@163.com>
2023-03-09 18:13:45 +0800

    tearup 和teardown

---
+++ Unittest/SetupTeardown.py
@@ -0,0 +1,36 @@
+import unittest
+from  selenium import  webdriver
+from  time import  sleep
+from  selenium.webdriver.common.by import By
+from  selenium import  webdriver
+from  time import  sleep
+from  selenium.webdriver.common.by import By
+
+class setupteardown(unittest.TestCase):
+
+    def tearDown(self):
+        print("end")
+
+    def setUp(self):
+        driver = webdriver.Chrome()
+        # 打开浏览器
+        driver.maximize_window()
+        # 浏览器窗口最大化
+        driver.get('https://www.baidu.com/')
+        # 浏览器打开baidu网站
+        sleep(2)
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertNotEqual('foo'.upper(), 'FOO2')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def testssss(self):
+        print("ssss")
+
+
+if __name__ == '__main__':
+    unittest.main()
