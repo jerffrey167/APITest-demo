67ffd4747c06a6298df2d17e55933ea9bd37598e
jerffrey <jerffrey_167@163.com>
2023-03-09 21:52:21 +0800

    examUI   unittest

--- Unittest/case/examUI.py
+++ Unittest/case/examUI.py
@@ -1,32 +1,38 @@
-from  selenium.webdriver.support.wait import WebDriverWait
 from  selenium import  webdriver
 from  time import  sleep
 from  selenium.webdriver.common.by import By
 import unittest
 
-driver = webdriver.Chrome()
-# 打开浏览器
-driver.maximize_window()
+class Test_UI(unittest.TestCase):
 
-driver.implicitly_wait(5)
-#浏览器窗口最大化
-driver.get('http://120.46.215.163:8102/#/login')
-#浏览器打开考试系统
-driver.find_element(By.XPATH,'//input[@type="text"]').send_keys('admin')
-#用户名
-driver.find_element(By.XPATH,'//input[@type="password"]').send_keys('admin')
-#密码
-driver.find_element(By.XPATH,'//button[@type="button"]').click()
-#登录按钮
+    @classmethod
+    def setUpClass(self):
+        global driver
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        driver.get('http://120.46.215.163:8102/#/login')
 
-driver.find_element(By.XPATH,"//*[starts-with(@style,'padding-left: 20px')]/span[text()='考试管理']").click()
-#点击考试管理模块
-sleep(1)
-driver.find_element(By.XPATH,"//*[starts-with(@style,'padding-left: 40px')]/span[text()='考试管理']").click()
-sleep(1)
-driver.find_element(By.XPATH,"//div[text()=' 超管A ']").click()
-sleep(1)
-driver.find_element(By.XPATH,"//span[text()='退出登录']//..").click()
+    def test_001login(self):
+        driver.find_element(By.XPATH, '//input[@type="text"]').send_keys('admin')
+        driver.find_element(By.XPATH, '//input[@type="password"]').send_keys('admin')
+        driver.find_element(By.XPATH, '//button[@type="button"]').click()
 
-driver.quit()
-#关闭浏览器
+
+    def test_002PageOpen(self):
+        driver.find_element(By.XPATH, "//*[starts-with(@style,'padding-left: 20px')]/span[text()='考试管理']").click()
+        sleep(1)
+        driver.find_element(By.XPATH, "//*[starts-with(@style,'padding-left: 40px')]/span[text()='考试管理']").click()
+
+    def test_003Lingout(self):
+        driver.find_element(By.XPATH, "//div[text()=' 超管A ']").click()
+        sleep(1)
+        driver.find_element(By.XPATH, "//span[text()='退出登录']//..").click()
+        sleep(1)
+
+    @classmethod
+    def tearDownClass(self):
+        driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
