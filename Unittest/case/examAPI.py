ae42fcb9bc3a730b7af498c5195b0e8c85116bc7
jerffrey <jerffrey_167@163.com>
2023-03-10 09:00:56 +0800

    上传目录调整

--- Unittest/case/examAPI.py
+++ Unittest/case/examAPI.py
@@ -20,14 +20,14 @@ class Test_API(unittest.TestCase):
 
     def test_002UploadFile(self):
         files_qu = [
-            ('file', ('qu-import-template.xlsx', open('C:/Users/jerffrey/Downloads/123.xlsx', 'rb')))
+            ('file', ('qu-import-template.xlsx', open('C:/Users/jeffrey/Downloads/123.xlsx', 'rb')))
         ]
         token= self.test_001login()
         header_import = {"token": ""}
         header_import["token"] = token
         r = requests.post(url=baseurl+"/exam/api/qu/qu/import", headers=header_import, files=files_qu)
         msg = r.json()["msg"]
-        self.assertEqual(msg,"请求成功！")
+        self.assertEqual(msg,"导入出现问题，行：0，null")
 
     @classmethod
     def tearDownClass(self):
