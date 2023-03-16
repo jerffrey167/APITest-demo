bebd65978ce0268d6a576acf055b2c80500f5e1e
jerffrey <jerffrey_167@163.com>
2023-03-17 02:42:38 +0800

    pytest

---
+++ Pytest/skip.py
@@ -0,0 +1,13 @@
+import pytest
+
+myskip=pytest.mark.skipif(1==1,reason='skip赋值给变量，可多处调用')
+class Test099(object):
+    @myskip
+    def test_one(self):
+        assert 1==2
+    def test_two(self):
+        print('test_02')
+        assert 1==1
+
+if __name__=='__main__':
+    pytest.main(['-rs'])
